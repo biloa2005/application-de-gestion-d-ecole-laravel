c4c4f04078700a41e03b556d272e63752c857aec
biloa2005 <biloaphilemon@gmail.com>
2026-01-25 03:10:57 +0100

    php laravel

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+use App\Http\Controllers\NiveauController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    Route::prefix("niveaux")->group(function(){
+        Route::get("/",[NiveauController::class, "index"])->name("niveaux");
+    });
+});

98a67cab5bdd8ba92063f395dc884cb489be0037
biloa2005 <biloaphilemon@gmail.com>
2026-02-03 03:09:34 +0100

    3/2/2026

--- app/Livewire/Settings.php
+++ app/Livewire/Settings.php
@@ -1,13 +1,27 @@
 <?php
-
 namespace App\Livewire;
+
+use App\Models\SchoolYear;
 
 use Livewire\Component;
 
+use Livewire\WithPagination;
 class Settings extends Component
 {
+    use WithPagination;
+    public $search="";
     public function render()
     {
-        return view('livewire.settings');
+        if(!empty($this->search)){
+              $schoolYearList= SchoolYear::where("school_year", 'like','%'.$this->search.'%')->paginate(10);
+
+
+        }else{
+  $schoolYearList= SchoolYear::paginate(10);
+
+        }
+      
+
+        return view('livewire.settings',compact("schoolYearList"));
     }
 }
--- resources/views/livewire/settings.blade.php
+++ resources/views/livewire/settings.blade.php
@@ -2,7 +2,11 @@
     <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
         <!-- titre et boutton creer-->
         <div class="flex justify-between items-center">
-            <h4>Liste des Annees scolaires</h4>
+            <input type="text" class="block rounded-md border-gray-300
+      
+        w-full
+        " placeholder="Recherche"
+        wire:model.live="search">
             <a href="{{route('settings.create_school_year')}}" class="bg-blue-500 rounded-md p-2 text-sm text-white">
                 Nouvelle Annee Scolaire
             </a>
@@ -11,10 +15,15 @@
 
         <div class="flex flex-col">
             <!-- MESSAGE QUI APPARAITRA APRES UNE OPERATION -->
-            <div class="block p-2 bg-green-500 text-white rounded-sm shadow-sm mt-2">
+            <!-- <div class="block p-2 bg-green-500 text-white rounded-sm shadow-sm mt-2">
                 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae quos beat
                 ae corporis accusantium quaerat deleniti rerum, dolores rem.
-            </div>
+            </div> -->
+            @if(Session::get("success"))
+    <div class="p-5">
+        <div class="block p-2 bg-green-500 text-white rounded-sm shadow-sm mt-2">{{Session::get("success")}}</div>
+    </div>
+     @endif
             <!-- styliser le tableau -->
              <div class="overflow-x-auto ">
                 <div class="py-4 inline-block min-w-full ">
@@ -22,28 +31,44 @@
                         <table class="min-w-full text-center">
                             <thead class="border-b bg-gray-50">
                                 <tr>
-                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">Libelle</th>
-                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">Libelle</th>
-                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">Libelle</th>
-                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">Libelle</th>
+                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">id</th>
+                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">Annee Scolaire</th>
+                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">Status</th>
+                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">Actions</th>
                                 </tr>
                             </thead>
                             <tbody>
+                                @forelse($schoolYearList as $item )
                                 <tr class="border-b-2 border-gray-100">
-                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">Libelle-exemple</th>
-                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">Libelle-exemple</th>
-                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">Libelle-exemple</th>
-                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">Libelle-exemple</th>
+                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">{{$item->id}}</th>
+                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">{{$item->school_year}}</th>
+                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">@if($item->active >= 1)
+                                                <span class="p-1 bg-green-400 text-sm text-white rouded-sm ">Active</span>                                        
+                                        @else
+                                             <span class="p-1 bg-red-400 text-sm text-white rouded-sm ">Inactive</span>
+                                        @endif
+
+                                    </th>
+                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">
+                                        @if($item->active>=1)
+                                                  <button class="p-2 text-white bg-red-400 rounded-sm text-sm ">Rendre Inatif</button>
+                                            @else
+                                            <button class="p-2 text-white bg-green-400 rounded-sm text-sm ">Rendre Actif</button>                   
+                                        @endif
+                                    </th>
                                 </tr>
-                                 <tr class="border-b-2 border-gray-100">
-                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">Libelle-exemple</th>
-                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">Libelle-exemple</th>
-                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">Libelle-exemple</th>
-                                    <th class="text-sm font-medium text-gray-900 px-6 py-6">Libelle-exemple</th>
-                                </tr>
+                                @empty
+                                @endforelse
+                            
+                              
+                              
                             </tbody>
                         </table>
                         <!-- pagination -->
+                        <div class="mt-1">
+                                {{$schoolYearList->links()}}
+                                </div>
+
                     </div>
                 </div>
              </div>
